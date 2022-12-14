023b64e9a75f909d382d78722e0ce89b5c1bcd52
Luiz Rioja <lulyrioja@gmail.com>
2022-12-14 18:32:42 -0300

    feat: create database and http modules

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+
+import { PrismaService } from './prisma/prisma.service';
+import { NotificationsRepository } from '../../application/repositories/notifications-repository';
+import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsRepository,
+    },
+  ],
+  exports: [NotificationsRepository],
+})
+export class DatabaseModule {}
