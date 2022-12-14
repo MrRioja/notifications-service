3dbcce8b5b4663766e7bd0abaa59af172ebc0f14
Luiz Rioja <lulyrioja@gmail.com>
2022-12-14 18:35:02 -0300

    feat(SendNotification): create use-case and test

---
+++ src/application/use-cases/send-notification.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface SendNotificationRequest {
+  content: string;
+  category: string;
+  recipientId: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+@Injectable()
+export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { category, content, recipientId } = request;
+
+    const notification = new Notification({
+      category,
+      recipientId,
+      content: new Content(content),
+    });
+
+    await this.notificationsRepository.create(notification);
+
+    return {
+      notification,
+    };
+  }
+}
