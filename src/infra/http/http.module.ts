700eccd5d614d001437e4ac67c39f83d9981c17a
Luiz Rioja <lulyrioja@gmail.com>
2022-12-15 14:08:07 -0300

    feat(HttpModule): add use-cases in providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../database/database.module';
-import { SendNotification } from '../../application/use-cases/send-notification';
+import { SendNotification } from '@application/use-cases/send-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 import { NotificationsController } from './controllers/notifications.controller';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    ReadNotification,
+    UnreadNotification,
+    CancelNotification,
+    GetRecipientNotifications,
+    CountRecipientNotifications,
+  ],
 })
 export class HttpModule {}
